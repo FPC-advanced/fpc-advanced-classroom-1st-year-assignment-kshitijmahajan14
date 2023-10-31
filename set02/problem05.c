bdeebe0a77384ada884fc791de7cc2b7923a6aab
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-10-31 12:09:17 +0530

    committed 10/31/2023, 12:09:16 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -7,5 +7,25 @@ int input(){
     scanf("%d", &n);
     return n;
 }
-int find_gcd(int a, int b);
-void output(int a, int b, int gcd);
+int find_gcd(int a, int b){
+    while(a!=b){
+    if(a>b){
+        a-=b;
+    }
+    else{
+        b-=a;
+    }
+    }
+    return a;
+}
+void output(int a, int b, int gcd){
+    printf("the gcd of %d and %d is %d", a,b,gcd);
+}
+
+int main(){
+    int n = input();
+    int n1 = input();
+    int gcd= find_gcd(n,n1);
+    output(n,n1,gcd);
+    return 0;
+}
