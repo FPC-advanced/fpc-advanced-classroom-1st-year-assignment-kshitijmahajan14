756b20de59a2a45e865a677d9f4923d7d4957af5
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-10-31 11:45:32 +0530

    committed 10/31/2023, 11:45:30 AM

--- set01/problem12.c
+++ set01/problem12.c
@@ -1,51 +1,53 @@
-#include<stdio.h>
+#include <stdio.h>
 
-typedef struct{
+typedef struct {
     float real;
     float imag;
 } Complex;
 
-int get_n(){
+int get_n() {
     int n;
-    printf("enter number of complex numbers to be added: ");
+    printf("Enter the number of complex numbers: ");
     scanf("%d", &n);
     return n;
 }
-Complex input_complex(){
+
+Complex input_complex() {
     Complex c;
-    printf("enter real part: ");
+    printf("Enter real part: ");
     scanf("%f", &c.real);
-    printf("enter imaginary part: ");
+    printf("Enter imaginary part: ");
     scanf("%f", &c.imag);
     return c;
 }
 
 void input_n_complex(int n, Complex c[n]) {
-    for(int i = 0; i < n; i++) {
-        printf("Enter complex number %d\n", i+1);
+    for(int i=0; i<n; i++) {
+        printf("Enter complex number %d", i+1);
         c[i] = input_complex();
     }
 }
 
-Complex add_n_complex(int n, Complex c[n]){
-    Complex sum;
-    for(int i; i<n; i++){
+Complex add_n_complex(int n, Complex c[n]) {
+    Complex sum = {0, 0};
+    for(int i=0; i<n; i++) {
         sum.real += c[i].real;
         sum.imag += c[i].imag;
     }
     return sum;
 }
-void output(Complex result){
-    printf("the sum of all complex numbers is %f is %fi", result.real, result.imag);
+
+void output(int n, Complex c[n], Complex result) {
+printf(" is (%f + %fi)", result.real, result.imag);
 }
 
-int main()
-{
+int main() {
     int n = get_n();
     Complex c[n];
-    input_complex();
-    Complex sum= add_n_complex(n,c);
-    output(sum);
+    input_n_complex(n, c);
+    Complex sum = add_n_complex(n, c);
+    output(n, c, sum);
     return 0;
 }
+
 
