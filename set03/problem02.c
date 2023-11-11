e9e906577a4bd250f8d3e5e5a16ebf0a011ba100
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-11-11 18:59:38 +0530

    committed 11/11/2023, 6:59:36 PM

---
+++ set03/problem02.c
@@ -0,0 +1,14 @@
+/*finding whether 3 pts form a triangle*/
+
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
+    printf("enter point 1 coordinates: ");
+    scanf("%f %f", x1, y1);
+    printf("enter point 1 coordinates: ");
+    scanf("%f %f", x1, y1);
+    printf("enter point 1 coordinates: ");
+    scanf("%f %f", x1, y1);
+
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
