32efa2db9227258caab59c9671050f5f0145dd74
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-11-11 18:54:07 +0530

    committed 11/11/2023, 6:54:06 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -19,7 +19,7 @@ void output(float x1, float y1, float x2, float y2, float distance){
 
 int main(){
     float x1,y1,x2,y2,distance;
-    input(&x1,&x2,&y1,&y2);
+    input(&x1,&y1,&x2,&y2);
     distance=find_distance(x1,y1,x2,y2);
     output(x1,y1,x2,y2,distance);
 
