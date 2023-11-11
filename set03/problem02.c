b5aebe0027538b9f6ba2fcc29e60778fa32bff67
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-11-11 19:00:08 +0530

    committed 11/11/2023, 7:00:06 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -4,11 +4,10 @@
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
     printf("enter point 1 coordinates: ");
     scanf("%f %f", x1, y1);
-    printf("enter point 1 coordinates: ");
-    scanf("%f %f", x1, y1);
-    printf("enter point 1 coordinates: ");
-    scanf("%f %f", x1, y1);
-
+    printf("enter point 2 coordinates: ");
+    scanf("%f %f", x2, y2);
+    printf("enter point 3 coordinates: ");
+    scanf("%f %f", x3, y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
