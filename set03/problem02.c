8a0cfe8dda359b8eee3ac2cc1ada318041f04c9f
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-11-11 19:18:11 +0530

    committed 11/11/2023, 7:18:10 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -30,3 +30,14 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
         printf("the 3 points (%f,%f),(%f,%f) and (%f,%f) do not form a triangle", x1,y1,x2,y2,x3,y3);
     }
 }
+
+int main(){
+    float x1,y1,x2,y2,x3,y3;
+    int result;
+
+    input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+    result = is_triangle(x1,y1,x2,y2,x3,y3);
+    output(x1,y1,x2,y2,x3,y3,result);
+
+    return 0;
+}
