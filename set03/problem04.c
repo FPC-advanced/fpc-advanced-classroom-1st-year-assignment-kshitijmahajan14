919fa656820d08572c12bf6196116dacd4b632a1
kshitijmahajan14 <kshitijs.bsc23@rvu.edu.in>
2023-11-12 15:31:20 +0530

    committed 11/12/2023, 3:31:19 PM

---
+++ set03/problem04.c
@@ -0,0 +1,13 @@
+/*find nth number in fibo sequence*/
+
+#include<stdio.h>
+int input(){
+    int n;
+    printf("enter nth number of fibonacci series: ");
+    scanf("%d", &n);
+    return n;
+}
+int find_fibo(int n){
+    
+}
+void output(int n, int fibo);
